e51966e70090c2511f7f1718b9cd092b3a48800f
mkuzin <m.kuzin@polymatica.ru>
2024-11-09 23:50:09 +0300

    up

--- internal/repository/version.go
+++ internal/repository/version.go
@@ -1,25 +1,34 @@
 package repository
 
 import (
+	"errors"
 	"github.com/Maksumys/db-migrator/internal/models"
 	"gorm.io/gorm"
 )
 
 func GetVersion(db *gorm.DB) (string, error) {
 	var row models.VersionModel
-	err := db.First(&row).Error
+	res := db.Find(&row)
 
-	switch err {
-	case gorm.ErrRecordNotFound:
-		return row.Version, ErrNotFound
-	default:
-		return row.Version, err
+	if res.Error != nil {
+		switch {
+		case errors.Is(res.Error, gorm.ErrRecordNotFound):
+			return "", ErrNotFound
+		default:
+			return "", res.Error
+		}
 	}
+
+	if res.RowsAffected == 0 {
+		return "", ErrNotFound
+	}
+
+	return row.Version, nil
 }
 
 func SaveVersion(db *gorm.DB, version string) error {
 	var row models.VersionModel
-	count := db.First(&row).RowsAffected
+	count := db.Find(&row).RowsAffected
 
 	if count == 0 {
 		_ = db.Create(&models.VersionModel{Version: version}).Error
